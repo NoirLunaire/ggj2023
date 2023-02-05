ae4a3ef6be6fe0439b6f98da82bce4625733e889
NoirLunaire <117916676+NoirLunaire@users.noreply.github.com>
2023-02-05 21:40:37 +0100

    adding a todo + naming convention in effects.go

---
+++ TODO.md
@@ -0,0 +1,4 @@
+# TODO
+- Redo time with actual timers, not TPS shenaningans
+- Redo effect storage and execution
+- Write a story :)
--- game/effects.go
+++ game/effects.go
@@ -7,6 +7,10 @@ func BuildTower (s *State) {
 	s.Tower = 1
 }
 
+// M = minus, P = Plus
+// Hap = Happiness, Mon = Money, Pop = Population
+// always in the above order
+
 func addEvents (s *State) {
 	s.EventPool = []int{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }
 }
