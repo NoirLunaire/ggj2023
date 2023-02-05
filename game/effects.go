3fe5e627d91cf8e4dff2dffc67428b17a0612f5c
NoirLunaire <117916676+NoirLunaire@users.noreply.github.com>
2023-02-05 19:15:44 +0100

    adding a lot of events

--- game/effects.go
+++ game/effects.go
@@ -8,7 +8,7 @@ func BuildTower (s *State) {
 }
 
 func addEvents (s *State) {
-	s.EventPool = []int{ 15 }
+	s.EventPool = []int{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 15 }
 }
 
 func P1Hap (s *State) {
@@ -59,4 +59,74 @@ func M5Mon (s *State) {
 
 func M3Hap (s *State) {
 	s.Happiness -= 3
+}
+
+func P4HapP10MonP3Pop (s *State) {
+	s.Happiness += 4
+	s.Money += 10
+	s.Population += 3
+}
+
+func M3HapM6Pop (s *State) {
+	s.Happiness -= 3
+	s.Population -= 6
+}
+
+func M6HapM3Pop (s *State) {
+	s.Happiness -= 6
+	s.Population -= 3
+}
+
+func P4HapM7Mon (s *State) {
+	s.Happiness += 4
+	s.Money -= 7
+}
+
+func P2HapM4Pop (s *State) {
+	s.Population -= 4
+	s.Happiness += 2
+}
+
+func P25Mon (s *State) {
+	s.Money += 25
+}
+
+func P50Mon (s *State) {
+	s.Money += 50
+}
+
+func M50Mon (s *State) {
+	s.Money -= 50
+}
+
+func M20Hap (s *State) {
+	s.Happiness -= 20
+}
+
+func P1HapM7MonP3Pop (s *State) {
+	s.Money -= 7
+	s.Happiness += 1
+	s.Population += 3
+}
+
+func M2HapP1MonM4Pop (s *State) {
+	s.Happiness -= 2
+	s.Money += 1
+	s.Population -= 4
+}
+
+func P2HapM4MonP1Pop (s *State) {
+	s.Happiness += 2
+	s.Money -= 4
+	s.Population += 1
+}
+
+func P1Mon (s *State) {
+	s.Money += 1
+}
+
+func M1HapP1MonM1Pop (s *State) {
+	s.Happiness -= 1
+	s.Money += 1
+	s.Population -= 1
 }
